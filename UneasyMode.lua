764adb1db6c3854c2472838bbbea5df7bf5fc008
FOCUS <focus_114514@qq.com>
2025-07-13 16:25:45 +0800

    Update UneasyMode.lua

--- UneasyMode.lua
+++ UneasyMode.lua
@@ -1,23 +1,34 @@
+if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 0 then
+    warn("You shouldn't be here. GET OUT!")
+    if replicatesignal then
+        replicatesignal(game.Players.LocalPlayer.Kill)
+    else
+        game.Players.LocalPlayer.Character.Humanoid.Health = 0
+    end
+    game.ReplicatedStorage.GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Error: Not The Right Place"
+    firesignal(game.ReplicatedStorage.RemotesFolder.DeathHint.OnClientEvent, {"You can only execute the script at Door 0.", "Please remember this!"}, 'Blue')
+end
+
 local caption = game.Players.LocalPlayer:WaitForChild("PlayerGui").MainUI.MainFrame.Caption
 caption.TextColor3 = Color3.fromRGB(0, 0, 0)
---Hello
+-- Hello
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Loading...",
-    Text = "You cannot escape",
+    Text = "You can't escape!",
     Duration = 5,
     Icon = "rbxassetid://12537555348"
 })
---Depth uneasy
+-- Depth uneasy
 local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
 
 ---====== Display achievement ======---
 achievementGiver({
     Title = "It's Cold Here...",
     Desc = "I felt scared.",
-    Reason = "Executed uneasy mode.",
+    Reason = "Executed \"Uneasy Mode\".",
     Image = "rbxassetid://17651731518"
 })
---anti two(Muhammad)
+-- anti two (Muhammad)
 if game:GetService("ReplicatedStorage"):FindFirstChild("LOVE") then
 
     require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You're already upset....", true)
@@ -26,327 +37,335 @@ if game:GetService("ReplicatedStorage"):FindFirstChild("LOVE") then
 end
 
 local Sadness = Instance.new("Part")
-
 Sadness.Name = "LOVE"
-
 Sadness.Parent = game:GetService("ReplicatedStorage")
 
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("You executed Uneasy Mode[V0.1].", true)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Uneasy Mode executed successfully!", true)
+task.wait(2)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Version: v0.2_Beta.", true)
+task.wait(2)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Made by LLGCAllIWantIsYou.", true)
+task.wait(2)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Additional Help: Focuslol666.", true)
 
 print(math.pi)
 print(math.pow(2, 12))
 
---A combination of Insanity fog mode and Anzor's mode
-wait(3)
+-- A combination of Insanity fog mode and Anzor's mode
+task.wait(3)
 
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).remind("You're feeling very uneasy right now.", true)
 
-wait(5)
+task.wait(5)
 
-require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).titlelocation("Uneasy_Place", true)
+coroutine.wrap(function()
+    game.ReplicatedStorage.GameData.LatestRoom.Changed:Connect(function()
+        if game.ReplicatedStorage.GameData.LatestRoom.Value == 1 then
+            require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).titlelocation("The Uneasy Place", true)
+        end
+    end)
+end)()
 
---Can't
+-- Can't
 game:GetService("TextChatService").ChatWindowConfiguration.Enabled = false
 game.SoundService.AmbientReverb = "Cave"
 
---A258
-coroutine.wrap(function()
+-- A258
+task.spawn(function()
     while true do
-        wait(99)
+        task.wait(99)
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(2)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/IR_A258.lua'))()
+        task.wait(2)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/IR_A258.lua'))()
     end
-end)()
+end)
 
---DoorsMaterials
+-- DoorsMaterials
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/Doors-Custom-Things/refs/heads/main/DoorsMaterials.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/Doors-Custom-Things/refs/heads/main/DoorsMaterials.lua"))()
 	
-end)()
+end)
 
---Lurk
-coroutine.wrap(function()
+-- Lurk
+task.spawn(function()
     while true do
-        wait(1008)
+        task.wait(1008)
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(521)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/Lurk.lua'))()
+        task.wait(521)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/Lurk.lua'))()
     end
-end)()
+end)
 
---doorssndaa
+-- doorssndaa
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/Doors-Custom-Things/refs/heads/main/DoorsSound.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/Doors-Custom-Things/refs/heads/main/DoorsSound.lua"))()
 	
-end)()
+end)
 
---Cave Ambience
+-- Cave Ambience
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/Cave-Ambience.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/Cave-Ambience.lua"))()
 	
-end)()
+end)
 
---Candle2
+-- Candle2
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/ca.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/ca.lua"))()
 	
-end)()
+end)
 
---NewSound
+-- New Sound
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/EntityUneasySound.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/EntityUneasySound.lua"))()
 	
-end)()
+end)
 
---Dear(A variant of dread)
-coroutine.wrap(function()
+-- Dear (A variant of dread)
+task.spawn(function()
     while true do
-        wait(math.random(300,530))
+        task.wait(math.random(300,530))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Dear.lua'))()
+        task.wait(1)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Dear.lua'))()
     end
-end)()
+end)
 
---Victrola(check78 but i fix it )
+-- Victrola (check78 but I fix it)
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Victrola.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Victrola.lua"))()
 	
-end)()
+end)
 
---Sprint
-
-coroutine.wrap(function()
-	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/DOORS/StaminaSystem.lua"))()
-	
-end)()
-
---Admiration
-coroutine.wrap(function()
-    while true do
-        wait(math.random(60,100))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(0.5)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Admiration.lua'))()
-    end
-end)()
-
---Galena
-coroutine.wrap(function()
-    while true do
-        wait(math.random(100,400))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(12)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Galena.lua'))()
-    end
-end)()
-
---Ahead(Anzor)
-coroutine.wrap(function()
-    while true do
-        wait(math.random(71,300))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(1)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Ahead.lua'))()
-    end
-end)()
-
--- pandemonium(dripcapybara)
-
-coroutine.wrap(function()
- 
-while true do
- 
-local sctm = math.random(4500,5600)
- 
-wait(sctm)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/PandemoniumProtected.lua"))() 
-
+-- Stamina System (FOCUS)
+if getgenv().Sprinting then
+    task.spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/RbxScripts/refs/heads/main/DOORS/StaminaSystem.lua"))()
+    end)
 end
 
-end)()
+-- Admiration
+task.spawn(function()
+    while true do
+        task.wait(math.random(60,100))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        task.wait(0.5)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Admiration.lua'))()
+    end
+end)
+
+-- Galena
+task.spawn(function()
+    while true do
+        task.wait(math.random(100,400))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        task.wait(12)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Galena.lua'))()
+    end
+end)
+
+-- Ahead (Anzor)
+task.spawn(function()
+    while true do
+        task.wait(math.random(71,300))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        task.wait(1)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Ahead.lua'))()
+    end
+end)
+
+-- Z-367 Pandemonium (dripcapybara)
+
+task.spawn(function()
+ 
+    while true do
+ 
+        local sctm = math.random(4500,5600)
+ 
+        task.wait(sctm)
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/DripCapybara/Doors-Mode-Remakes/refs/heads/main/PandemoniumProtected.lua"))() 
+
+    end
+
+end)
 
 -- SHY
 
-coroutine.wrap(function()
+task.spawn(function()
  
-while true do
+    while true do
  
-local sctm = math.random(40,100)
+        local sctm = math.random(40,100)
  
-wait(sctm)
+        task.wait(sctm)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/ShY.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/ShY.lua"))()
 
-end
+    end
 
-end)()
+end)
 
---materials map(iCherrykardes)
+-- materials map (iCherrykardes)
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/icherryKardes.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/icherryKardes.lua"))()
 	
-end)()
+end)
 
 -- ambience sound1
 
-coroutine.wrap(function()
+task.spawn(function()
  
-while true do
+    while true do
  
-local sb = math.random(60,100)
+        local sb = math.random(60,100)
  
-wait(sb)
+        task.wait(sb)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/MP3player.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/green-fog/refs/heads/main/MP3player.lua"))()
 
-end
-
-end)()
-
---eyes scary
-
-coroutine.wrap(function()
-	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/ScaryEyes.lua"))()
-	
-end)()
-
---House number prefix(X-001)
-
-coroutine.wrap(function()
-	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/S-.lua"))()
-	
-end)()
-
---Specter
-coroutine.wrap(function()
-    while true do
-        wait(math.random(100,420))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Specter.lua'))()
     end
-end)()
 
---OG Rush
+end)
 
-coroutine.wrap(function()
+-- eyes scary
+
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/OgRush.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/ScaryEyes.lua"))()
 	
-end)()
+end)
 
---NM ambush
+-- House number prefix (X-001)
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/NMAmbush.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/S-.lua"))()
 	
-end)()
+end)
 
---Scream
-coroutine.wrap(function()
+-- Specter
+task.spawn(function()
     while true do
-        wait(math.random(50,514))
+        task.wait(math.random(100,420))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(0.5)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Scream.lua'))()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Specter.lua'))()
     end
-end)()
+end)
 
---LightRed
+-- OG Rush
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Light.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/OgRush.lua"))()
 	
-end)()
+end)
 
---HaltGlitch(glitchambush made it)
+-- NM ambush
 
-coroutine.wrap(function()
+task.spawn(function()
 	
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/GlitchHalt.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/NMAmbush.lua"))()
 	
-end)()
+end)
 
---The BOILED ONE[Credits to OG Hunger]
-coroutine.wrap(function()
+-- Scream
+task.spawn(function()
     while true do
-        wait(math.random(250,500))
+        task.wait(math.random(50,514))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Theboiledone.lua'))()
+        task.wait(0.5)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Scream.lua'))()
     end
-end)()
+end)
+
+-- LightRed
+
+task.spawn(function()
+	
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Light.lua"))()
+	
+end)
+
+-- HaltGlitch (glitch ambush made it)
+
+task.spawn(function()
+	
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/GlitchHalt.lua"))()
+	
+end)
+
+-- The BOILED ONE [Credits to OG Hunger]
+task.spawn(function()
+    while true do
+        task.wait(math.random(250,500))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Theboiledone.lua'))()
+    end
+end)
 
 -- A-55
 
-coroutine.wrap(function()
+task.spawn(function()
  
-while true do
- 
-local ok = math.random(290,377)
- 
-wait(ok)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/A-55.lua"))() 
-
-end
-
-end)()
-
---Overseer eyes[check78]CF78M
-coroutine.wrap(function()
     while true do
-        wait(math.random(500,991))
-        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
-        wait(0)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/OverseerEyes.lua'))()
+ 
+        local ok = math.random(290,377)
+ 
+        task.wait(ok)
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/A-55.lua"))() 
+
     end
-end)()
 
---Bliss but bad
-coroutine.wrap(function()
+end)
+
+-- Overseer eyes [check78] C87FM
+task.spawn(function()
+    while true do
+        task.wait(math.random(500,991))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        task.wait()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/OverseerEyes.lua'))()
+    end
+end)
+
+-- Bliss but bad
+task.spawn(function()
  
-while true do
+    while true do
  
-local sm = math.random(900,1000)
+        local sm = math.random(900,1000)
  
-wait(sm)
+        task.wait(sm)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Bliss.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/Bliss.lua"))()
 
-end
+    end
 
-end)()
+end)
 
---RTX(idk fog)
-if getgenv().RTX then
-
-    coroutine.wrap(function()
+-- RTX (idk fog)
+if getgenv().RTX then -- Default: true
+    task.spawn(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/RTX.lua"))()
-    end)()
+    end)
 end
 
-wait(5)
+task.wait(5)
 
 loadstring(request({Url="https://raw.githubusercontent.com/LLGCAllIWantIsYou/urban-broccoli/refs/heads/main/WarningUI.lua"}).Body)()
 
